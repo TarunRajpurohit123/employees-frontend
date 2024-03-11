0568b976a24ea65268962b56170bb6c4c8f4b233
Tarun <tarunrajpurohitcreator@gmail.com>
2024-03-11 08:16:21 +0530

    demo csv file download functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,18 @@ import { Drawer, EForm, Fluid, Table, Uploader } from "./components";
 import { Button } from "antd";
 import { DownloadOutlined, UploadOutlined, UserAddOutlined } from "@ant-design/icons";
 import { useSelector } from "react-redux";
+import fileDownload from "js-file-download";
 
 function App() {
-  const isOpen = useSelector((state)=>{return state.drawerSlice.open})
+  const isOpen = useSelector((state)=>{return state.drawerSlice.open});
+
+  // download demo csv
+  // download excle file
+  const downloadFile = () => {
+    let data =
+      "EmployeeName,EmployeeStatus,JoiningDate,BirthDate,Skills,SalaryDetails,Address and other custom info";
+    fileDownload(data, "demo-employee.csv");
+  };
 
   return (
     <React.Fragment>
@@ -17,7 +26,7 @@ function App() {
           <Drawer heading={"Bulk Upload"} label={<>Bulk Upload <UploadOutlined /></>}  btnStyles={{marginLeft:"1rem",background:"var(--dark)"}}>
             <Uploader/>
           </Drawer>
-          <Button style={{marginLeft:"1rem"}}>Demo Csv <DownloadOutlined/></Button>
+          <Button style={{marginLeft:"1rem"}} onClick={()=>{downloadFile();}}>Demo Csv <DownloadOutlined/></Button>
         </div>
         <Table />
       </Fluid>
